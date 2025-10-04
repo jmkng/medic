c8f638318c8fbbab223f7b2a87a2648c72c1a22c
Jon Koenig <jkoenig@outlook.com>
2025-10-04 14:26:24 -0600

    add `medic_host_uptime`
    also, stop printing in library functions,
    and change returned error code to -1 to match sysctl.

--- host.h
+++ host.h
@@ -1,1 +1,25 @@
-long medic_host_boot_time();
+/**
+ * Retrieves the system boot time as a UNIX timestamp.
+ *
+ * This function queries the system kernel for the time at which the
+ * host machine was booted.
+ *
+ * The caller should cache the result.
+ *
+ * @return
+ *   The boot time as seconds since the UNIX epoch (1970-01-01 00:00:00 UTC),
+ *   or -1 if an error occurred while retrieving the boot time.
+ */
+long medic_host_boot_time(void);
+
+/**
+ * Returns the system uptime in seconds.
+ *
+ * This function queries the system kernel for the time at which the
+ * host machine was booted.
+ *
+ * @return
+ *   The uptime in seconds,
+ *   or -1 if an error occurred while retrieving the boot time.
+ */
+long medic_host_uptime(void);
--- host_darwin.c
+++ host_darwin.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <sys/sysctl.h>
+#include <sys/types.h>
 
-long medic_host_boot_time()
+// SYSCTL(3)
+// https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/sysctl.3.html
+
+long medic_host_boot_time(void)
 {
     struct timeval boottime;
     size_t size = sizeof(boottime);
 
     int mib[2] = { CTL_KERN, KERN_BOOTTIME };
     int code = sysctl(mib, 2, &boottime, &size, NULL, 0) != 0;
-    if (code != 0) {
-        perror("sysctl+kern.boottime");
-        return 0;
-    }
+    if (code == -1)
+        return -1;
 
     return boottime.tv_sec;
+}
+
+long medic_host_uptime(void)
+{
+    long boot_time = medic_host_boot_time();
+    if (boot_time == -1)
+        return -1;
+
+    long now = time(NULL);
+    long diff_secs = now - boot_time;
+    return diff_secs;
 }
