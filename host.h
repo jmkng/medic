6354d458924d2824f47efe0754300cdcf1e26510
Jon Koenig <jkoenig@outlook.com>
2025-10-04 17:22:56 -0600

    update docs

--- host.h
+++ host.h
@@ -3,8 +3,9 @@
 /**
  * Returns the system boot time as a UNIX timestamp.
  *
- * This function queries the system kernel for the time at which the host machine was booted.
- * The caller should cache the result.
+ * Queries the system kernel on each call.
+ * The result is unlikely to change during program execution,
+ * callers may wish to cache the value.
  *
  * @return
  *      The boot time as seconds since the UNIX epoch (1970-01-01 00:00:00 UTC),
@@ -15,8 +16,9 @@ long medic_host_boot_time(void);
 /**
  * Returns the system uptime in seconds.
  *
- * This function queries the system kernel for the time at which the
- * host machine was booted.
+ * Queries the system kernel on each call.
+ * The result is unlikely to change during program execution,
+ * callers may wish to cache the value.
  *
  * @return
  *      The uptime in seconds,
@@ -27,25 +29,23 @@ long medic_host_uptime(void);
 /**
  * Copies the system kernel version to buffer as a null terminated string.
  *
- * This function queries the system kernel for the kernel version.
- * The caller should cache the result.
+ * Queries the system kernel on each call.
+ * The result is unlikely to change during program execution,
+ * callers may wish to cache the value.
  *
  * @return
- *      The string assigned to buffer is always null-terminated.
- *      The caller should ensure the buffer is large enough to hold the version,
- *      256 bytes may be sufficient.
+ *      0 on success, or -1 on error.
  */
 int medic_kernel_version(char* buffer, size_t size);
 
 /**
  * Copies the system kernel type to buffer as a null terminated string.
  *
- * This function queries the system kernel for the kernel type.
- * The caller should cache the result.
+ * Queries the system kernel on each call.
+ * The result is unlikely to change during program execution,
+ * callers may wish to cache the value.
  *
  * @return
- *      The string assigned to buffer is always null-terminated.
- *      The caller should ensure the buffer is large enough to hold the version,
- *      256 bytes may be sufficient.
+ *      0 on success, or -1 on error.
  */
 int medic_kernel_type(char* buffer, size_t size);
